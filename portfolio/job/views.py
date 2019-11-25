0220a222e10efa2744d8ac5dc48818bbf2af7731
Laura <lpvera22@gmail.com>
2019-11-25 19:33:24 -0300

    changing theme

--- portfolio/job/views.py
+++ portfolio/job/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'job/home.html')
+    return render(request,'job/index.html')
+
+def job_details(request):
+    return render(request,'job/detail.html')
+
 
